1d7b881d13eacbb0c127b82f776528633754de46
peliksdev <jfcastillo.dev@gmail.com>
2025-07-30 21:05:35 +0800

    sana macount as contributions

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,9 +1,14 @@
 let toDoList = [];
-function addToDoList () {
-  const inputElement = document.querySelector('.js-todo-input');
+function addToDoList (selector) {
+  const inputElement = document.querySelector(selector);
 
   toDoList.push(inputElement.value);
   inputElement.value = '';
+  
+  if (selector === '.js-todo-input1') {
+    console.log(toDoList);
+  } else if (selector === '.js-todo-input2') {
+    document.querySelector('.js-todo-name').innerHTML = toDoList;
+  }
 
-  console.log(toDoList);
 }
