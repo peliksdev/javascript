0d6131e86d6bbe8346382b6aa0e4cd25511cb274
peliksdev <jfcastillo.dev@gmail.com>
2025-07-31 15:26:59 +0800

    todo list

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,4 +1,5 @@
 let toDoList = [];
+
 function addToDoList () {
   const inputElement = document.querySelector('.js-todo-input');
   const name = inputElement.value;
@@ -6,4 +7,47 @@ function addToDoList () {
   toDoList.push(name);
   inputElement.value = '';
   console.log(toDoList);
+}
+
+let toDoList2 = [];
+let dateList = [];
+renderTodoList();
+
+function addToDoList2 () {
+  const inputElement = document.querySelector('.js-todo-input2');
+  const inputDateElement = document.querySelector('.js-date');
+  const name = inputElement.value;
+  const date = inputDateElement.value;
+
+  toDoList2.push(name);
+  dateList.push(date);
+  inputElement.value = '';
+  inputDateElement.value = '';
+  renderTodoList();
+}
+
+function renderTodoList () {
+  let todoHTML = '';
+  for (let i = 0; i < toDoList2.length && i < dateList.length; i++) {
+    const todo = toDoList2[i];
+    const date = dateList[i];
+    const html = `<p>${todo} 
+
+                  <span>
+                    ${date}
+                  </span>
+
+                  <button class="js-delete-btn"
+                  onclick = "deleteTask(${i});">Delete</button>
+
+                  </p>`;
+    todoHTML += html;
+  }
+
+  document.querySelector('.js-todo-container').innerHTML = todoHTML;
+}
+
+function deleteTask (index) {
+  toDoList2.splice(index,1);
+  renderTodoList();
 }
